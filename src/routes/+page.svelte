aa17c13136dd25b277ef6096c4970e8add81a2e3
SimonLindi <simon@lindenmann.net>
2025-01-07 09:22:42 +0100

    vor bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
-<h1>Welcome simons to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<a href="/trips/">zu den trips</a>
+<div class="container mt-5 text-center">
+    <h1 class="text-primary">Bootstrap Test</h1>
+    <img src="/src/images/driving_car.gif" alt="Driving Car" class="img-fluid my-3" />
+    <button class="btn btn-success">Teste Bootstrap Button</button>
+  </div>
+  
